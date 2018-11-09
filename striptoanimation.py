8e2c39b9bbd8ce58c0eb6bc79e438e4036037e6c
Alan Steremberg <alan.steremberg@gmail.com>
2018-11-09 15:13:19 -0800

    adding

--- striptoanimation.py
+++ striptoanimation.py
@@ -1,6 +1,6 @@
-from PIL import Image
+from PIL import Image,ImageDraw
 
-im = Image.open("smallerimage.png")
+im = Image.open("try2.png")
 newim = Image.new("RGBA",(8,8))
 width,height = im.size
 num=0
@@ -8,15 +8,17 @@ x=0
 images=[]
 while x< width-8:
   x=x+2
+  draw = ImageDraw.Draw(newim)
+  draw.rectangle(((0, 0), (8, 8)), outline="#ffffffff",fill="#FFFFFFFF")
   newim.paste(im,(x*-1,0))
   num=num+1
   name='output/new_'+"{:03d}".format(num)+'.gif'
-  #print(name)
+  print(name)
   images.append(newim.copy())
-  #newim.save(name)
+  newim.save(name,transparency=0)
 
 images[0].save('anitest.gif',
                save_all=True,
                append_images=images[1:],
-               duration=200,
+               duration=200,transparency=0,
                loop=0)
